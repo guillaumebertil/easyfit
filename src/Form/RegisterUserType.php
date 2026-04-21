e6edae9b9e27e156503523f82b78790088ab324f
guillaumebertil <guillaume.bertil@gmail.com>
2026-04-21 11:21:46 +0200

    feat: système d'authentification complet (inscription, connexion, déconnexion)

---
+++ src/Form/RegisterUserType.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class RegisterUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr'  => [
+                    'placeholder' => 'John'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr'  => [
+                    'placeholder' => 'Doe'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail',
+                'attr'  => [
+                    'placeholder' => 'john.doe@example.com'
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                    'constraints' => [
+                        new Assert\NotBlank(),
+                        new Assert\Length([
+                            'min' => 6,
+                            'minMessage' => 'Le mot de passe doit contenir au minimum {{ limit }} caractères',
+                            'max' => 16,
+                            'maxMessage' => 'Le mot de passe ne doit pas dépasser {{ limit }} caractères'
+                        ]),
+                        new Assert\Regex([
+                            'pattern' => '/[A-Z]/',
+                            'message' => 'Le mot de passe doit contenir au moins une majuscule'
+                        ])
+                    ],
+                    'hash_property_path' => 'password',
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe'
+                ],
+                'mapped' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer un compte'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'constraints' => [
+
+            ],
+            'data_class' => User::class,
+        ]);
+    }
+}
